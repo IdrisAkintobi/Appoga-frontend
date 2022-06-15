63a20ead16ef3895be3e41bd235e8b64e0ac4d17
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-06-15 23:28:12 +0100

    Refactored page

--- src/pages/Articles/Articles.tsx
+++ src/pages/Articles/Articles.tsx
@@ -1,30 +1,59 @@
 import articleStyle from "./Articles.module.css";
 import TopStories from "../../components/Articles/TopStories";
 import Categories from "../../components/Articles/Categories";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../../components/Loader/Loader";
 
-const data: any = [
-  {
-    category: "Technology",
-    details: "This is a dummy data, It is supposed to be avery long one",
-    imgUrl: "./sample.png",
-    createdAt: "2011-10-05T14:48:00.000Z",
+const options = {
+  method: "GET",
+  url: "https://appoga.herokuapp.com/blog",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Iml0c2VydmljZS5saXZlcHJvamVjdEBnbWFpbC5jb20iLCJpYXQiOjE2NTQ3ODgzNjJ9.aW2pTAna5Se8dKReZeSkCejkJAeIKxJjGayalFP4Wcc",
   },
-  {
-    category: "Science",
-    details: "This is a dummy data, It is supposed to be avery long one",
-    imgUrl: "./sample.png",
-    createdAt: "2019-10-05T14:48:00.000Z",
-  },
-  {
-    category: "Accountancy",
-    details: "This is a dummy data, It is supposed to be avery long one",
-    imgUrl: "./sample.png",
-    createdAt: "2021-10-05T14:48:00.000Z",
-  },
-];
+};
+
+type iniValue = {
+  _id: string;
+  title: string;
+  message: string;
+  description: string;
+  summary: string;
+  category: string;
+  username: string;
+  createdAt: string;
+  updatedAt: string;
+};
 
 const Article = () => {
-  return (
+  const [data, setData] = useState<Array<iniValue>>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.request(options);
+        if (response.status === 200) {
+          setIsLoading(false);
+        }
+        setData(response.data);
+
+        console.log(data);
+      } catch (error) {
+        setIsLoading(false);
+        console.log(error);
+      }
+    })();
+    // eslint-disable-next-line
+  }, []);
+
+  const loading = (
+    <div className={articleStyle.spinner}>
+      <Loader />
+    </div>
+  );
+  const actualRender = (
     <>
       <div className={articleStyle.hero}>
         <div className={articleStyle.hero_text}>See what is going on</div>
@@ -33,6 +62,7 @@ const Article = () => {
       <Categories data={data} />
     </>
   );
+  return <>{isLoading ? loading : actualRender}</>;
 };
 
 export default Article;
