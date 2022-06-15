8fc6c91deaaf427c9e65ca40c38237301313636e
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-06-15 23:24:08 +0100

    Added ChatboxIcons

---
+++ src/assets/svg/ChatboxIcons.tsx
@@ -0,0 +1,65 @@
+const CloseIcon = (
+  <>
+    <svg
+      fill="#fff"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="20px"
+      height="20px"
+    >
+      <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z" />
+    </svg>
+  </>
+);
+
+const SendIcon = (
+  <>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      width="16"
+      height="16"
+      viewBox="0 0 256 256"
+      xmlSpace="preserve"
+    >
+      <g
+        transform="translate(128 128) scale(7.21 7.21)"
+        style={{ height: "24px" }}
+      >
+        <g
+          style={{
+            stroke: "none",
+            strokeWidth: 0,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 10,
+            fill: "none",
+            fillRule: "nonzero",
+            opacity: 1,
+          }}
+          transform="translate(-17.55 -17.55) scale(0.39 0.39)"
+        >
+          <polygon
+            points="0,14.69 0,39.65 51,45 0,50.35 0,75.31 90,45 "
+            style={{
+              stroke: "none",
+              strokeWidth: 1,
+              strokeDasharray: "none",
+              strokeLinecap: "butt",
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 10,
+              fill: "rgb(244, 237, 237)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+            transform="  matrix(1 0 0 1 0 0) "
+          />
+        </g>
+      </g>
+    </svg>
+  </>
+);
+
+export { CloseIcon, SendIcon };
